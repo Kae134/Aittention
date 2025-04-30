ca70c43831330aeb304b69cfb0fb07004d384f83
Alexis G <alexis.gontier03@gmail.com>
2025-04-30 16:51:20 +0200

    🔥 Refactor ThemeToggle component to use dedicated functions for theme changes

--- frontend/components/theme/theme-toggle.tsx
+++ frontend/components/theme/theme-toggle.tsx
@@ -15,23 +15,33 @@ import {
 export function ThemeToggle() {
   const { setTheme } = useTheme()
 
+  const hundleThemeLight = () => {
+    setTheme("light")
+  }
+  const hundleThemeDark = () => {
+    setTheme("dark")
+  }
+  const hundleThemeSystem = () => {
+    setTheme("system")
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="ghost" size="icon" className="cursor-pointer">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={hundleThemeLight}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={hundleThemeDark}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={hundleThemeSystem}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
