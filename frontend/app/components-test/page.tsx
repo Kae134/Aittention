6e98f92d91950e374a2ae0e97687de5cef1454bb
gontier <alexis.gontier03@laposte.net>
2025-05-13 14:58:02 +0200

    ✨ Ajout du composant SonnerDemo avec des boutons pour les notifications

---
+++ frontend/app/components-test/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { toast } from "sonner"
+import { Button } from "@/components/shadcn-ui/button"
+import { ThemeToggle } from "@/components/theme/theme-toggle"
+import Link from "next/link"
+
+export default function SonnerDemo() {
+
+  const handleSuccess = () => {
+    toast.success("Success!")
+  }
+  const handleError = () => {
+    toast.error("Error!")
+  }
+  const handleInfo = () => {
+    toast.info("Info!")
+  }
+  const handleWarning = () => {
+    toast.warning("Warning!")
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 w-full h-screen">
+      <div className="flex gap-4">
+        <Button variant="outline" onClick={handleSuccess}>
+          Success
+        </Button>
+        <Button variant="outline" onClick={handleError}>
+          Error
+        </Button>
+        <Button variant="outline" onClick={handleInfo}>
+          Info
+        </Button>
+        <Button variant="outline" onClick={handleWarning}>
+          Warning
+        </Button>
+      </div>
+      <ThemeToggle />
+      <Link href="/mvp" className="text-blue-500 hover:underline">
+        Go to MVP
+      </Link>
+    </div>
+  )
+}
