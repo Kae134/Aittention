9ecc9265620275733eae4b07048a06bed8002b51
Alexis G <alexis.gontier03@gmail.com>
2025-05-21 11:05:44 +0200

    ✨ Ajout du composant ImageReveal pour une comparaison d'images et mise à jour des dépendances

--- frontend/app/components-test/page.tsx
+++ frontend/app/components-test/page.tsx
@@ -1,9 +1,7 @@
 "use client"
 
 import { toast } from "sonner"
-import { Button } from "@/components/shadcn-ui/button"
-import { ThemeToggle } from "@/components/theme/theme-toggle"
-import Link from "next/link"
+import ImageReveal from "@/components/ui/image-reveal"
 
 export default function SonnerDemo() {
 
@@ -22,24 +20,10 @@ export default function SonnerDemo() {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4 w-full h-screen">
-      <div className="flex gap-4">
-        <Button variant="outline" onClick={handleSuccess}>
-          Success
-        </Button>
-        <Button variant="outline" onClick={handleError}>
-          Error
-        </Button>
-        <Button variant="outline" onClick={handleInfo}>
-          Info
-        </Button>
-        <Button variant="outline" onClick={handleWarning}>
-          Warning
-        </Button>
-      </div>
-      <ThemeToggle />
-      <Link href="/mvp" className="text-blue-500 hover:underline">
-        Go to MVP
-      </Link>
+      <ImageReveal
+        firstImage="/site_web_overlay.png"
+        secondImage="/site_web.png"
+      />
     </div>
   )
 }
