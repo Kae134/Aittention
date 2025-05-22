ffb6a015cfc5b0cad082b5ba557d5486c98ad050
Jeck0v <arnaud.fischer75@gmail.com>
2025-05-22 15:35:14 +0200

    Terraform pre-configuration of the Spacelift connection to Google Cloud

---
+++ backend/app/infra/main.tf
@@ -0,0 +1,33 @@
+resource "google_iam_workload_identity_pool" "spacelift_pool" {
+  workload_identity_pool_id = "spacelift-pool"
+  location                  = "global"
+  display_name              = "Spacelift OIDC Pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "spacelift_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.spacelift_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "spacelift-provider"
+  location                           = "global"
+  display_name                       = "Spacelift Provider"
+
+  oidc {
+    issuer_uri = "https://<TON_WORKSPACE>.app.spacelift.io"
+  }
+
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+  }
+}
+
+resource "google_service_account" "spacelift_sa" {
+  account_id   = "spacelift-ci"
+  display_name = "Spacelift CI Service Account"
+}
+
+resource "google_service_account_iam_binding" "allow_wif" {
+  service_account_id = google_service_account.spacelift_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principalSet://iam.googleapis.com/projects/${var.gcp_project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.spacelift_pool.workload_identity_pool_id}/attribute.subject/*"
+  ]
+}
---
+++ backend/app/infra/variables.tf
@@ -0,0 +1,12 @@
+variable "gcp_project" {
+
+}
+variable "gcp_region" {
+
+}
+variable "gcp_svc_key" {
+
+}
+variable "gcp_project_number" {
+  
+}
