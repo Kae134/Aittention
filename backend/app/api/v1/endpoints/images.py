5fc2862a45af8da34ac56a54df501760656e9aeb
Kae134 <louisdondey@gmail.com>
2025-06-11 09:30:24 +0200

    Added cleaned code from yesterday

--- backend/app/api/v1/endpoints/images.py
+++ backend/app/api/v1/endpoints/images.py
@@ -3,6 +3,10 @@ from fastapi.responses import StreamingResponse
 from app.utils.image import validate_image
 from app.core.storage import store_image, get_image_by_id, get_user_images
 from io import BytesIO
+from urllib.parse import quote
+
+import unicodedata
+import re
 
 router = APIRouter()
 
@@ -12,7 +16,15 @@ async def post_image(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail="Invalid image type")
 
     content = await file.read()
-    image_id = store_image(file.filename, content)
+    
+    name = file.filename
+    
+    name = unicodedata.normalize("NFD", name)
+    name = re.sub(r"[\u0300-\u036f]", "", name)
+    
+    name = re.sub(r"[^a-zA-Z0-9_.-]", "_", name)
+
+    image_id = store_image(name, content)
 
     return {"message": "Image received", "image_id": str(image_id)}
 
@@ -23,10 +35,16 @@ def get_image(id: str):
     content = image["data"]
     filename = image["filename"]
 
+    encoded_filename = quote(filename)
+
+    headers = {
+        "Content-Disposition": f"inline; filename*=UTF-8''{encoded_filename}"
+    }
+
     return StreamingResponse(
         BytesIO(content),
         media_type="image/jpeg",
-        headers={"Content-Disposition": f"inline; filename={filename}"}
+        headers=headers
     )
 
 @router.get("/users/{user_id}/images")
